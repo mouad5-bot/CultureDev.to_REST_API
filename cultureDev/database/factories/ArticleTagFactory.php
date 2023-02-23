3d14a0a5a10042a8006b01af7fc0c081435359ec
Hicham El Arfaouy <hicham.elarfaoui.1@gmail.com>
2023-02-23 13:16:59 +0100

    generate factories and seeders for all tables (fake data)

--- cultureDev/database/factories/ArticleTagFactory.php
+++ cultureDev/database/factories/ArticleTagFactory.php
@@ -3,9 +3,10 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article_tag>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ArticleTag>
  */
 class ArticleTagFactory extends Factory
 {
@@ -16,8 +17,11 @@ class ArticleTagFactory extends Factory
      */
     public function definition()
     {
+        $articles_id = DB::table('articles')->pluck('id')->toArray();
+        $tags_id = DB::table('tags')->pluck('id')->toArray();
         return [
-            //
+            'article_id' => fake()->randomElement($articles_id),
+            'tag_id' => fake()->randomElement($tags_id),
         ];
     }
 }
--- cultureDev/database/seeders/DatabaseSeeder.php
+++ cultureDev/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(RoleSeeder::class);
+        $this->call(CategorySeeder::class);
+        User::factory(5)->create();
+        $this->call(ArticleSeeder::class);
+        $this->call(CommentSeeder::class);
+        $this->call(TagSeeder::class);
+        $this->call(ArticleTagSeeder::class);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
